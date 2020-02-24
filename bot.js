00e4cc65d784f1a0e0f054eb821bd1549a9cc1c6
ekaterina olivier <ekaterina.olivier@epitech.eu>
2020-02-25 00:06:03 +0100

    init

---
+++ bot.js
@@ -0,0 +1,23 @@
+// Run dotenv
+require('dotenv').config();
+
+// Import libraries
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Event listener when a user connect to the server
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Event listener when a user sends a message in the chat
+client.on('message', msg => {
+
+  // We check the message content and looks for the word "ping", so we can have the bot respond "pong"
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+// Initialize bot by connecting to the server
+client.login(process.env.DISCORD_TOKEN);
